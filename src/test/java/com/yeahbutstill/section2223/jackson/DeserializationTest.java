a579cfb75f8bbce5d3ec9b673268d8a6320d5125
yeahbutstill <mayadani28@gmail.com>
2023-04-04 00:32:40 +0700

    serialization inclusion

--- src/test/java/com/yeahbutstill/section2223/jackson/DeserializationTest.java
+++ src/test/java/com/yeahbutstill/section2223/jackson/DeserializationTest.java
@@ -3,13 +3,18 @@ package com.yeahbutstill.section2223.jackson;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.yeahbutstill.section2223.domain.dao.Address;
 import com.yeahbutstill.section2223.domain.dao.Person;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 class DeserializationTest {
+
+    private static final Logger LOGGER = Logger.getLogger(DeserializationTest.class.getName());
 
     @Test
     void testDeserialization() throws JsonProcessingException {
@@ -42,6 +47,27 @@ class DeserializationTest {
         Assertions.assertEquals("123 Main Street", person.getAddress().getStreet());
         Assertions.assertEquals("New York", person.getAddress().getCity());
         Assertions.assertEquals(List.of("Coding"), person.getHobbies());
+    }
+
+    @Test
+    void testSerialization() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper()
+                .configure(SerializationFeature.INDENT_OUTPUT, true);
+
+        Person person = new Person();
+        person.setId(1L);
+        person.setName("John");
+        person.setHobbies(List.of("Coding", "Reading"));
+
+        Address address = new Address();
+        address.setStreet("123 Main Street");
+        address.setCity("New York");
+        address.setCountry("USA");
+        person.setAddress(address);
+
+        String json = objectMapper.writeValueAsString(person);
+        Assertions.assertNotNull(json);
+        LOGGER.info(json);
     }
 
 }
---
+++ src/test/java/com/yeahbutstill/section2223/jackson/SerializationInclusionTest.java
@@ -0,0 +1,28 @@
+package com.yeahbutstill.section2223.jackson;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yeahbutstill.section2223.domain.dao.Person;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.logging.Logger;
+
+class SerializationInclusionTest {
+
+    private static final Logger LOGGER = Logger.getLogger(SerializationInclusionTest.class.getName());
+
+    @Test
+    void testSerializationInclusion() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        Person person = new Person();
+        person.setId(1L);
+        person.setName("John");
+
+        String json = objectMapper.writeValueAsString(person);
+        Assertions.assertNotNull(json);
+        LOGGER.info(json);
+    }
+
+}
